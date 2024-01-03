3c6dd563c3db37d8ed49b8593de6ad90725f144f
cyberguli <guglielmo.padula@aruba.it>
2024-01-03 21:47:57 +0100

    added ParMonDMD

---
+++ README.md
@@ -0,0 +1,9 @@
+# Lid Cavity Non intrusive ROM Analysis
+In this repo, some common used models are compared on a [Lid](https://github.com/guglielmopadula/LidCavity) [Cavity](https://github.com/guglielmopadula/LidCavity) [Dataset](https://github.com/guglielmopadula/LidCavity).
+The models, with their main characteristics and performances, are summed up here.
+
+|   Model     |         Features        |rel u train error|rel u test error|rel v train error|rel v test error|rel p train error|rel p test error| 
+|-------------|-------------------------|-----------------|----------------|-----------------|----------------|-----------------|----------------|
+|ParMonDMD+RBF|Fixed space and time grid|7.0e-06          |2.0e-05         |5.5e-06          |2.2e-05         |7.2e-06          |6.0e-05
+
+
---
+++ models/monolithic_pydmd_train_rbf.py
@@ -0,0 +1,66 @@
+from lidcavity import LidCavity 
+from pydmd import ParametricDMD, DMD
+from ezyrb import POD, RBF, GPR, KNeighborsRegressor
+import numpy as np
+data=LidCavity(10)
+params_train=data.params_train
+params_test=data.params_test
+u_train=data.V_train[:,:,:,0].transpose(1,2).numpy()
+u_test=data.V_test[:,:,:,0].transpose(1,2).numpy()
+dmd=DMD(svd_rank=-1)
+rom=POD('svd',rank=400)
+interpolator=RBF()
+
+pdmd_monolithic_velocity_u = ParametricDMD(dmd, rom, interpolator)
+pdmd_monolithic_velocity_u.fit(
+    u_train, params_train
+)
+
+
+pdmd_monolithic_velocity_u.parameters=params_train.reshape(-1,1)
+rel_u_train_error=np.linalg.norm(pdmd_monolithic_velocity_u.reconstructed_data-u_train)/np.linalg.norm(u_train)
+
+pdmd_monolithic_velocity_u.parameters=params_test.reshape(-1,1)
+rel_u_test_error=np.linalg.norm(pdmd_monolithic_velocity_u.reconstructed_data-u_test)/np.linalg.norm(u_test)
+
+v_train=data.V_train[:,:,:,1].transpose(1,2).numpy()
+v_test=data.V_test[:,:,:,1].transpose(1,2).numpy()
+dmd=DMD(svd_rank=-1)
+rom=POD('svd',rank=400)
+interpolator=RBF()
+
+pdmd_monolithic_velocity_v = ParametricDMD(dmd, rom, interpolator)
+pdmd_monolithic_velocity_v.fit(
+    v_train, params_train
+)
+
+pdmd_monolithic_velocity_v.parameters=params_train.reshape(-1,1)
+rel_v_train_error=np.linalg.norm(pdmd_monolithic_velocity_v.reconstructed_data-v_train)/np.linalg.norm(v_train)
+
+pdmd_monolithic_velocity_v.parameters=params_test.reshape(-1,1)
+rel_v_test_error=np.linalg.norm(pdmd_monolithic_velocity_v.reconstructed_data-v_test)/np.linalg.norm(v_test)
+
+
+p_train=data.V_train[:,:,:,2].transpose(1,2).numpy()
+p_test=data.V_test[:,:,:,2].transpose(1,2).numpy()
+dmd=DMD(svd_rank=-1)
+rom=POD('svd',rank=400)
+interpolator=RBF()
+
+pdmd_monolithic_velocity_p = ParametricDMD(dmd, rom, interpolator)
+pdmd_monolithic_velocity_p.fit(
+    p_train, params_train
+)
+
+pdmd_monolithic_velocity_p.parameters=params_train.reshape(-1,1)
+rel_p_train_error=np.linalg.norm(pdmd_monolithic_velocity_p.reconstructed_data-p_train)/np.linalg.norm(p_train)
+
+pdmd_monolithic_velocity_p.parameters=params_test.reshape(-1,1)
+rel_p_test_error=np.linalg.norm(pdmd_monolithic_velocity_p.reconstructed_data-p_test)/np.linalg.norm(p_test)
+
+print("rel train error of u_1 is", "{:.1E}".format(rel_u_train_error))
+print("rel test error of u_1 is", "{:.1E}".format(rel_u_test_error))
+print("rel train error of u_2 is", "{:.1E}".format(rel_v_train_error))
+print("rel test error of u_2 is", "{:.1E}".format(rel_v_test_error))
+print("rel train error of p is", "{:.1E}".format(rel_p_train_error))
+print("rel test error of p is", "{:.1E}".format(rel_p_test_error))
