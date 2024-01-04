793b0ed0dfe22c5d0cc568a74a8ac5189f1cfaf4
cyberguli <guglielmo.padula@aruba.it>
2024-01-04 17:51:57 +0100

    added GPY

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Lid Cavity Non intrusive ROM Analysis
 In this repo, some common used models are compared on a [Lid](https://github.com/guglielmopadula/LidCavity) [Cavity](https://github.com/guglielmopadula/LidCavity) [Dataset](https://github.com/guglielmopadula/LidCavity).
-The models, with their main characteristics and performances, are summed up here.
+
+To install the necessary requirements do:
+
+    $ pip install GPy sklearn pydmd scipy git+https://github.com/mathLab/EZyRB 
+
+The models, with their main characteristics and 
+performances, are summed up here.
+
 
 |   Model     |         Features                                       |rel u train error|rel u test error|rel v train error|rel v test error|rel p train error|rel p test error| 
 |-------------|--------------------------------------------------------|-----------------|----------------|-----------------|----------------|-----------------|----------------|
@@ -8,5 +15,7 @@ The models, with their main characteristics and performances, are summed up here
 |ParParDMD+RBF|Fixed space grid and variable time grid with fixed steps|1.1e-02          |1.0e-02         |2.8e-02          |2.6e-02         |1.9e-03          |1.9e-03         |
 |ParMonDMD+KNR|Fixed space grid and variable time grid with fixed steps|2.4e-03          |2.3e-03         |2.4e-03          |2.5e-03         |3.1e-03          |2.6e-03         |
 |ParParDMD+KNR|Fixed space grid and variable time grid with fixed steps|1.2e-02          |1.1e-02         |2.8e-02          |2.6e-02         |3.6e-03          |3.2e-03         |
+|ParMonDMD+GPY|Fixed space grid and variable time grid with fixed steps|2.4e-04          |2.2e-04         |2.5e-04          |2.3e-04         |1.1e-03          |9.5e-04         |
+|ParParDMD+GPY|Fixed space grid and variable time grid with fixed steps|1.1e-02          |1.0e-02         |2.8e-02          |2.7e-02         |1.9e-03          |1.9e-03         |
 
 
---
+++ models/monolithic_pydmd_train_gpy.py
@@ -0,0 +1,82 @@
+from lidcavity import LidCavity 
+from pydmd import ParametricDMD, DMD
+from ezyrb import POD
+import GPy
+import numpy as np
+
+class GPR():
+    def __init__(self):
+        self.kernel=GPy.kern.RBF(input_dim=1, variance=1., lengthscale=1.)
+    
+    def fit(self,x,y):
+        x_r=np.real(x)
+        y_r=np.real(y)
+        self.model=GPy.models.GPRegression(x_r,y_r,self.kernel)
+        self.model.optimize()
+
+    def predict(self,x):
+        x_r=np.real(x)
+        return self.model.predict(x_r)[0]
+
+data=LidCavity(10)
+params_train=data.params_train
+params_test=data.params_test
+u_train=data.V_train[:,:,:,0].transpose(1,2).numpy()
+u_test=data.V_test[:,:,:,0].transpose(1,2).numpy()
+dmd=DMD(svd_rank=-1)
+rom=POD('svd',rank=400)
+interpolator=GPR()
+
+pdmd_monolithic_velocity_u = ParametricDMD(dmd, rom, interpolator)
+pdmd_monolithic_velocity_u.fit(
+    u_train, params_train
+)
+
+
+pdmd_monolithic_velocity_u.parameters=params_train.reshape(-1,1)
+rel_u_train_error=np.linalg.norm(pdmd_monolithic_velocity_u.reconstructed_data-u_train)/np.linalg.norm(u_train)
+
+pdmd_monolithic_velocity_u.parameters=params_test.reshape(-1,1)
+rel_u_test_error=np.linalg.norm(pdmd_monolithic_velocity_u.reconstructed_data-u_test)/np.linalg.norm(u_test)
+
+v_train=data.V_train[:,:,:,1].transpose(1,2).numpy()
+v_test=data.V_test[:,:,:,1].transpose(1,2).numpy()
+dmd=DMD(svd_rank=-1)
+rom=POD('svd',rank=400)
+interpolator=GPR()
+
+pdmd_monolithic_velocity_v = ParametricDMD(dmd, rom, interpolator)
+pdmd_monolithic_velocity_v.fit(
+    v_train, params_train
+)
+
+pdmd_monolithic_velocity_v.parameters=params_train.reshape(-1,1)
+rel_v_train_error=np.linalg.norm(pdmd_monolithic_velocity_v.reconstructed_data-v_train)/np.linalg.norm(v_train)
+
+pdmd_monolithic_velocity_v.parameters=params_test.reshape(-1,1)
+rel_v_test_error=np.linalg.norm(pdmd_monolithic_velocity_v.reconstructed_data-v_test)/np.linalg.norm(v_test)
+
+
+p_train=data.V_train[:,:,:,2].transpose(1,2).numpy()
+p_test=data.V_test[:,:,:,2].transpose(1,2).numpy()
+dmd=DMD(svd_rank=-1)
+rom=POD('svd',rank=400)
+interpolator=GPR()
+
+pdmd_monolithic_velocity_p = ParametricDMD(dmd, rom, interpolator)
+pdmd_monolithic_velocity_p.fit(
+    p_train, params_train
+)
+
+pdmd_monolithic_velocity_p.parameters=params_train.reshape(-1,1)
+rel_p_train_error=np.linalg.norm(pdmd_monolithic_velocity_p.reconstructed_data-p_train)/np.linalg.norm(p_train)
+
+pdmd_monolithic_velocity_p.parameters=params_test.reshape(-1,1)
+rel_p_test_error=np.linalg.norm(pdmd_monolithic_velocity_p.reconstructed_data-p_test)/np.linalg.norm(p_test)
+
+print("rel train error of u_1 is", "{:.1E}".format(rel_u_train_error))
+print("rel test error of u_1 is", "{:.1E}".format(rel_u_test_error))
+print("rel train error of u_2 is", "{:.1E}".format(rel_v_train_error))
+print("rel test error of u_2 is", "{:.1E}".format(rel_v_test_error))
+print("rel train error of p is", "{:.1E}".format(rel_p_train_error))
+print("rel test error of p is", "{:.1E}".format(rel_p_test_error))
